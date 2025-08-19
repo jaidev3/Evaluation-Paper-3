4fa825f187bb78f2ff5a357cec097876476f95a9
JaiDev <jaidevyadav720@gmail.com>
2025-08-19 17:57:08 +0530

    rag implemented

--- backend/rag.py
+++ backend/rag.py
@@ -1,8 +1,89 @@
 from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chat_models import init_chat_model
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langgraph.graph import START, StateGraph
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langchain_community.document_loaders import PyPDFLoader
+from IPython.display import Image, display
+from langchain import hub
+
 load_dotenv()
 
-# from langch
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings,
+    persist_directory="./chroma_langchain_db",
+)
+
+loader = PyPDFLoader("./docs/ExerciseBook.pdf")
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+
+
+
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+# prompt  = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."
+
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
+
+example_messages = prompt.invoke(
+    {"context": "(context goes here)", "question": "(question goes here)"}
+).to_messages()
+
+assert len(example_messages) == 1
+print(example_messages[0].content)
+
+
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+# from IPython.display import Image, display
+
+# display(Image(graph.get_graph().draw_mermaid_png()))
+
+result = graph.invoke({"question": "What is Cable Bench Press?"})
+
+print(f"Context: {result['context']}\n\n")
+print(f"Answer: {result['answer']}")
 
 # load the document
 # extract the text

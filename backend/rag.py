af79d4b5348311ed40877e6c211d3d93e09f3d2e
JaiDev <jaidevyadav720@gmail.com>
2025-08-19 17:02:31 +0530

    setting up rag

---
+++ backend/rag.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+# from langch
+
+
+# load the document
+# extract the text
+
+# split the text/ chunking
+# embedding 
+# store to chromedb
+# on hitting endpoint of chat it will cal rag
+# embedd the query asked by user 
+# retrive the most similar chunks
+#  augment to llm along with query
+# get answer 
+# send in response
